9c68c92469be7512d33de0f6194e3b9959fa99b3
Marta Wierzbicka-Rogacka <marta.wierzbicka@wp.eu>
2015-10-21 21:48:24 +0200

    no message

---
+++ test.py
@@ -0,0 +1,24 @@
+
+print(
+"""
+	______________
+	|	    |
+	|	    O
+	|	  /-+-\\
+	|	 /  |  \\
+	|	    |
+	|	   | |
+	|	   | |
+	|
+--------------
+"""
+)
+
+# spersonalizowane przywitanie
+name = raw_input("Czesc. Jak masz na imie?  ")
+name = str(name)
+
+print "hello", name
+print "Zaczynamy gre wisielec! Powodzenia!!"
+
+raw_input("\n\nAby zakonczyc gre, nacisnij klawisz Enter")
